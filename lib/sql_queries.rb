3e875de313ecb743cdc8d39234e0bfd89a9028a0
Haley DeKeyser <hcd09@live.com>
2019-06-12 15:52:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,9 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   #users.age
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM pledges
-  LEFT OUTER JOIN users"
+  INNER JOIN users
+  ON pledges.user_id = users.id
+  "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
