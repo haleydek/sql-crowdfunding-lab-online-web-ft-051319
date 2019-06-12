04c969f22825807637d22cddd9159e0cfd905315
Haley DeKeyser <hcd09@live.com>
2019-06-12 16:29:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,8 +26,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  #users.name
-  #SUM(pledges.amount) group by pledges.user_id
   "SELECT users.name, SUM(pledges.amount)
   FROM pledges
   INNER JOIN users
@@ -37,7 +35,11 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount)
+FROM pledges
+INNER JOIN projects
+ON pledges.project_id = projects.id
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
