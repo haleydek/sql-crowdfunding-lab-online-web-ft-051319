3f1ea4a6efea07c7844b956dc6f45f6760a94e0f
Haley DeKeyser <hcd09@live.com>
2019-06-12 15:33:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,6 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+  #pledges table: sum of amounts per 
 "Write your SQL query Here"
 end
 
